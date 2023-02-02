72041a53ec848fe49975b60c81cccccf67456580
FaMaVa <faithvaz@Faiths-MacBook-Air.local>
2023-02-01 20:09:05 -0700

    add shiny toggle

--- client/src/components/PokeAdd/index.js
+++ client/src/components/PokeAdd/index.js
@@ -11,6 +11,7 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import Form from 'react-bootstrap/Form';
 
 //import from css -Faith
 import './style.css';
