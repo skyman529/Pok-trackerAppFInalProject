51ff825235bf1e395b7d6165969b5da7682b8305
Mntnbassfan <108299929+Mntnbassfan@users.noreply.github.com>
2023-02-01 18:45:25 -0700

    deleted files

--- work-from-24/server-models/models/index.js
+++
@@ -1,6 +0,0 @@
-const User = require('./User');
-const Poke = require('./Poke');
-const Shiny = require('./Shiny');
-const Order = require('./Order');
-
-module.exports = { User, Poke, Shiny, Order };
