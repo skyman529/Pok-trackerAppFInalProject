49ef14e20b2f865d3e213dd8afd8cf28bf233ffd
FaMaVa <faithvaz@Faiths-MacBook-Air.local>
2023-02-02 12:57:40 -0700

    updated queries.js in utils

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -19,8 +19,8 @@ export const QUERY_USER = gql`
 `;
 
 //Will query all pokes to choose from (data labels may change!) -Faith 
-export const QUERY_ALL_POKES = gql`
-  query getPokes {
+export const QUERY_POKEMONS = gql`
+  query getPokemons {
     pokemon {
       _id
       name
@@ -31,13 +31,19 @@ export const QUERY_ALL_POKES = gql`
 `;
 
 //Will query one poke to choose from (data labels may change and we could add more data to this!!) -Faith 
-export const QUERY_SINGLE_POKE = gql`
+export const QUERY_SINGLE_POKEMON = gql`
   query getSinglePoke($pokemonId: ID!) {
     pokemon(pokemonId: $pokemonId) {
       _id
       name
       type
       image
+      comments {
+        _id
+        commentText
+        commentAuthor
+        createdAt
+      }
     }
   }
 `;
