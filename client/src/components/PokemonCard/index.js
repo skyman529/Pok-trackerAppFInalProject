a48e3b1a707f3b00404f7fb92da31f53f78231c5
FaMaVa <faithvaz@Faiths-MacBook-Air.local>
2023-02-06 17:47:26 -0700

    added User List and updated single poke

--- client/src/components/PokemonCard/index.js
+++ client/src/components/PokemonCard/index.js
@@ -1,50 +1,44 @@
-//This component is to render the Poke Cards List to add (data labels may change!) -Faith
-
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 import Badges from '../Badges/index';
 import AddButton from '../AddButton/index';
 import Auth from '../../utils/auth';
 
-import { Card, Col, Row } from 'react-bootstrap';
+import {Card, Col, Row} from 'react-bootstrap';
 
 
-const PokemonList = ({ pokemons }) => {
-  if (pokemons) {
-    console.log(pokemons);
-  }
+const PokemonList = ({
+  id,
+  name,
+  type,
+  image,
+}) => {
 
   return (
     <div>
       {Auth.loggedIn() ? (
         <>
-          {pokemons &&
-            pokemons.map((pokemon) => (
               <Row xs={1} md={3} className="g-4 justify-content-md-center">
                 <Col md="3">
                   <Card style={{ width: '18rem' }}>
-                    {/* <Card.Img variant="top" src={pokemon.image} alt="example" /> */}
+                    {/* <Card.Img variant="top" src={image} alt="example" /> */}
                     <Card.Body>
                       {/* <Card.Text  id='pokeCard'>{pokemon.id}</Card.Text> */}
-                      <Link to={`/thoughts/${pokemon._id}`}>
-                        <Card.Title id='pokeCard'>{pokemon.name}</Card.Title>
-                      </Link>
+                      <Card.Title id='pokeCard'>{name}</Card.Title>
                       <Badges
-                        types={pokemon.type}
+                        types={type}
                       ></Badges>
-                      <br></br>
+                      <br></br>                      
                       <AddButton
-                        id={pokemon.id}
-                        name={pokemon.name}
-                        type={pokemon.type}
-                        image={pokemon.image}>
+                        id={id}
+                        name={name}
+                        type={type}
+                        image={image}>
                       </AddButton>
                     </Card.Body>
                   </Card>
                 </Col>
               </Row>
-            ))}
         </>
       ) : (
         <p>
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Navigate, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 
-import PokemonList from '../components/PokemonList';
+import UserPokemonList from '../components/UserPokemonList/index';
 
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
 
@@ -27,8 +27,7 @@ const Profile = () => {
   if (!user?.username) {
     return (
       <h4>
-        You need to be logged in to see this. Use the navigation links above to
-        sign up or log in!
+        You need to login!
       </h4>
     );
   }
@@ -41,11 +40,11 @@ const Profile = () => {
         </h2>
 
         <div className="col-12 col-md-10 mb-5">
-          <PokemonList
+          <UserPokemonList
             pokemons={user.pokemons}
             title={`${user.username}'s pokemons...`}
             showTitle={false}
-            showUsername={false}
+            // showUsername={false}
           />
         </div>
         {!userParam && (
